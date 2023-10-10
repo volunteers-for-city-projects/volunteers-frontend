c20884fc6f07590fb257abe1da816ba37be1ff0b
DBKai <dbkai@ya.ru>
2023-10-10 23:21:27 +0500

    feat: Added fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import './vendor/fonts/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
