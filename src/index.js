0a491b44979841c08af0d7b251e6f38b43ac1bdc
EvgeniiyaR <tulpansik@mail.ru>
2023-10-11 20:42:57 +0300

    feat: add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
@@ -7,6 +8,8 @@ import './vendor/fonts/fonts.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</React.StrictMode>
 );
