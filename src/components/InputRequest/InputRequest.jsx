bee87a64bf752729c13ca4e7526aea272590ab75
EvgeniiyaR <tulpansik@mail.ru>
2023-10-14 03:35:21 +0300

    feat(inputrequest): add validation input and styles

--- src/components/InputRequest/InputRequest.jsx
+++ src/components/InputRequest/InputRequest.jsx
@@ -1,16 +1,63 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { PatternFormat } from 'react-number-format';
 import './InputRequest.scss';
 
-function InputRequest({ type, htmlFor, label, placeholder }) {
+function InputRequest({
+	name,
+	type,
+	htmlFor,
+	label,
+	placeholder,
+	error,
+	value,
+	handleChange,
+	isMask,
+	submitCount,
+}) {
+	const [isFocus, setIsFocus] = useState(true);
+
 	return (
 		<label className="input-request" htmlFor={htmlFor}>
 			{label}
-			<input
-				className="input-request__input"
-				type={type}
-				id={htmlFor}
-				placeholder={placeholder}
-			/>
+			{isMask ? (
+				<PatternFormat
+					className={`input-request__input ${
+						(!isFocus && error) || submitCount === 1
+							? 'input-request__input_error'
+							: ''
+					}`}
+					value={value}
+					format="+7 (###) ###-##-##"
+					placeholder={placeholder}
+					mask="_"
+					onChange={handleChange}
+					name={name}
+					onBlur={() => {
+						setIsFocus(false);
+					}}
+				/>
+			) : (
+				<input
+					className={`input-request__input ${
+						(!isFocus && error) || submitCount === 1
+							? 'input-request__input_error'
+							: ''
+					}`}
+					name={name}
+					type={type}
+					id={htmlFor}
+					placeholder={placeholder}
+					value={value}
+					onChange={handleChange}
+					onBlur={() => {
+						setIsFocus(false);
+					}}
+				/>
+			)}
+			<span className="input-request__error">
+				{(!isFocus && error) || (submitCount === 1 && error)}
+			</span>
 		</label>
 	);
 }
@@ -20,10 +67,18 @@ InputRequest.propTypes = {
 	label: PropTypes.string,
 	htmlFor: PropTypes.string.isRequired,
 	type: PropTypes.string.isRequired,
+	error: PropTypes.string,
+	name: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	handleChange: PropTypes.func.isRequired,
+	isMask: PropTypes.bool,
+	submitCount: PropTypes.number.isRequired,
 };
 
 InputRequest.defaultProps = {
 	label: '',
+	error: '',
+	isMask: false,
 };
 
 export default InputRequest;
