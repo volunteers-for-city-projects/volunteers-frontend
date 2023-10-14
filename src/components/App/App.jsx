324c9a47b7b145749e0109ef5a8a2669a00c9e3a
EvgeniiyaR <tulpansik@mail.ru>
2023-10-14 03:40:32 +0300

    feat(app): move logic from header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Request from '../Request/Request';
+import FormRequest from '../FormRequest/FormRequest';
+import ModalConfirm from '../ModalConfirm/ModalConfirm';
 
 function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const [isOpenConfirmLogout, setIsOpenConfirmLogout] = useState(false);
+	const navigate = useNavigate();
+
+	const handleConfirmLogout = () => {
+		setIsOpenConfirmLogout(!isOpenConfirmLogout);
+	};
+
+	const closeConfirm = () => {
+		setIsOpenConfirmLogout(!isOpenConfirmLogout);
+	};
+
+	const handleLogout = (event) => {
+		event.preventDefault();
+		setIsOpenConfirmLogout(!isOpenConfirmLogout);
+		navigate('/');
+		setIsLoggedIn(false);
+	};
+
 	return (
 		<>
-			<Header />
+			<Header
+				isLoggedIn={isLoggedIn}
+				handleConfirmLogout={handleConfirmLogout}
+			/>
 			<div>
 				<p>App component</p>
 			</div>
-			<Request />
+			<FormRequest />
+			<Outlet />
 			<Footer />
+			<ModalConfirm
+				isOpen={isOpenConfirmLogout}
+				onSubmitOk={handleLogout}
+				onClickExit={closeConfirm}
+				closeConfirm={closeConfirm}
+			/>
 		</>
 	);
 }
