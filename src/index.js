1ccc8c16d2871e956c46d93dc864d1680265d0a5
EvgeniiyaR <tulpansik@mail.ru>
2023-10-14 03:38:00 +0300

    feat: add routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import './vendor/fonts/fonts.css';
+import router from './routes/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<RouterProvider router={router}>
 			<App />
-		</BrowserRouter>
+		</RouterProvider>
 	</React.StrictMode>
 );
