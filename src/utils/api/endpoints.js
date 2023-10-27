a0de65751bdeebe1a8a73f6648e56202a64f83d4
EvgeniiyaR <tulpansik@mail.ru>
2023-10-27 23:50:57 +0300

    feat: add endpoints for signup

---
+++ src/utils/api/endpoints.js
@@ -0,0 +1,18 @@
+export const URL = 'http://better-together.acceleratorpracticum.ru/api/';
+
+export const ENDPOINT_MAIN_PAGE_NEWS = 'news/';
+export const ENDPOINT_MAIN_PAGE_PLATFORM_ABOUT = 'platform_about/';
+export const ENDPOINT_MAIN_PAGE_FEEDBACK = 'feedback/';
+export const ENDPOINT_MAIN_PAGE_SEARCH = 'search';
+
+export const ENDPOINT_CITIES = 'cities/';
+export const ENDPOINT_SKILLS = 'skills/';
+export const ENDPOINT_USERS = 'users/';
+export const ENDPOINT_VOLUNTEERS = 'volunteers/';
+export const ENDPOINT_ORGANIZATIONS = 'organizations/';
+export const ENDPOINT_MEDIA = 'media/';
+
+export const ENDPOINT_SIGNIN = `${URL}auth/token/login/`;
+export const ENDPOINT_ABOUT_ME = `${URL}auth/me/`;
+export const ENDPOINT_RESET_PASSWORD = `${URL}auth/reset_password/`;
+export const ENDPOINT_CHANGE_PASSWORD = `${URL}auth/change_password/`;
