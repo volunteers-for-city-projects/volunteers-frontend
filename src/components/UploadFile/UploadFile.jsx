ff60bcdedec247a5ddf405b86b780da9ff51b085
EvgeniiyaR <tulpansik@mail.ru>
2023-11-01 23:21:05 +0300

    feat: add photo upload

--- src/components/UploadFile/UploadFile.jsx
+++ src/components/UploadFile/UploadFile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './UploadFile.scss';
 
@@ -8,7 +8,7 @@ export default function UploadFile({
 	type,
 	placeholder,
 	value,
-	setSelectedFile,
+	setFieldValue,
 	inputSize,
 	disabled,
 	required,
@@ -16,35 +16,47 @@ export default function UploadFile({
 	submitCount,
 	...restProps
 }) {
+	const [image, setImage] = useState('');
+
 	const handleFileChange = (event) => {
 		const file = event.target.files[0];
-		setSelectedFile(file);
+		const reader = new FileReader();
+
+		reader.onload = function () {
+			const base64Data = reader.result;
+			setFieldValue('photo', base64Data);
+			setImage(base64Data);
+		};
+
+		reader.readAsDataURL(file);
 	};
 
 	return (
-		<div>
-			<label htmlFor={name} className="label-file label-file_type-photo">
-				{required ? `${label}*` : label}
-			</label>
-
-			<input
-				name={name}
-				type={type}
-				placeholder={placeholder}
-				className="input-file input-file_type-photo"
-				required={required}
-				accept="image/*" // Укажите типы файлов, которые разрешено загружать
-				onChange={handleFileChange}
-				{...restProps}
-			/>
-		</div>
+		<>
+			{image && <img className="input-file__image" src={image} alt="" />}
+			<div>
+				<label htmlFor={name} className="label-file label-file_type-photo">
+					{required ? `${label}*` : label}
+				</label>
+				<input
+					name={name}
+					type={type}
+					placeholder={placeholder}
+					className="input-file input-file_type-photo"
+					required={required}
+					accept="image/*"
+					onChange={handleFileChange}
+					{...restProps}
+				/>
+			</div>
+		</>
 	);
 }
 
 UploadFile.propTypes = {
 	name: PropTypes.string.isRequired,
 	value: PropTypes.string,
-	setSelectedFile: PropTypes.func,
+	setFieldValue: PropTypes.func,
 	label: PropTypes.string.isRequired,
 	type: PropTypes.string.isRequired,
 	inputSize: PropTypes.oneOf(['small', 'medium', 'large', 'photo']),
@@ -62,6 +74,6 @@ UploadFile.defaultProps = {
 	required: false,
 	error: '',
 	submitCount: 0,
-	setSelectedFile: () => {},
+	setFieldValue: () => {},
 	value: '',
 };
