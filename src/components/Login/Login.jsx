ed1ff837b23b8c54d7086cb941dd4a3f810169bf
Vasiliy Konovalov <vvkon13@gmail.com>
2023-11-02 00:22:31 +0400

    feat(crumbs router login): bread crumbs

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigate, useOutletContext } from 'react-router-dom';
 import './Login.scss';
 import { apiLogin } from '../../utils/api/login-route';
 import { signIn, getUserInformation } from '../../utils/api/login';
+import { Crumbs } from '../Crumbs/Crumbs';
 
 function Login() {
 	const { isLoading, setIsLoading, setCurrentUser, setIsLoggedIn, setModal } =
@@ -85,14 +86,17 @@ function Login() {
 	};
 
 	return (
-		<Outlet
-			context={{
-				isLoading,
-				handleSignIn,
-				handlePasswordReset,
-				handleSaveChanges,
-			}}
-		/>
+		<>
+			<Crumbs />
+			<Outlet
+				context={{
+					isLoading,
+					handleSignIn,
+					handlePasswordReset,
+					handleSaveChanges,
+				}}
+			/>
+		</>
 	);
 }
 
