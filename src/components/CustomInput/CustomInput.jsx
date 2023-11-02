a4c01cb6f71d821d55295d39f9a64683fbee851c
DBKai <dbkai@ya.ru>
2023-11-02 23:55:53 +0500

    feat(custominput): added custom input for add project

---
+++ src/components/CustomInput/CustomInput.jsx
@@ -0,0 +1,70 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import './CustomInput.scss';
+import clsx from 'clsx';
+
+export default function CustomInput({
+	label,
+	placeholder,
+	type,
+	handleChange,
+	error,
+	required,
+	...props
+}) {
+	const [isFocused, setIsFocused] = React.useState(false);
+
+	const changeName = useCallback(
+		(value) => {
+			handleChange(value);
+		},
+		[handleChange]
+	);
+
+	return (
+		<div className="input__container">
+			{label.length > 0 && (
+				<label htmlFor="input" className="input__label">
+					{`${label}${required && '*'}`}
+				</label>
+			)}
+			<input
+				type={type}
+				placeholder={placeholder}
+				className={clsx('input__field', {
+					'input__field-error': isFocused && error?.length > 0,
+				})}
+				required={required}
+				onChange={changeName}
+				onBlur={() => {
+					setIsFocused(true);
+				}}
+				{...props}
+			/>
+
+			<span
+				className={clsx('input__error-message', {
+					'input__error-message_show': isFocused && error?.length > 0,
+				})}
+			>
+				{error}
+			</span>
+		</div>
+	);
+}
+
+CustomInput.propTypes = {
+	label: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	type: PropTypes.string.isRequired,
+	handleChange: PropTypes.func,
+	error: PropTypes.string,
+	required: PropTypes.bool,
+};
+
+CustomInput.defaultProps = {
+	placeholder: '',
+	required: false,
+	error: '',
+	handleChange: () => {},
+};
