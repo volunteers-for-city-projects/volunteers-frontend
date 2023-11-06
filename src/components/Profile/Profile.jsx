acd8310dd07d88f20d7d5f60350881408ffa3921
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2023-11-06 15:42:38 +0300

    fix(profile): update profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 
 import './Profile.scss';
 
-// import { useOutletContext } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
 
-// import ProfileVolunteer from '../ProfileVolunteer/ProfileVolunteer';
-// import ProfileVolunteerEdit from '../ProfileVolunteerEdit/ProfileVolunteerEdit';
+import ProfileVolunteer from '../ProfileVolunteer/ProfileVolunteer';
+import ProfileVolunteerEdit from '../ProfileVolunteerEdit/ProfileVolunteerEdit';
 import ProfileOrganization from '../ProfileOrganization/ProfileOrganization';
 import ProfileOrganizationEdit from '../ProfileOrganizationEdit/ProfileOrganizationEdit';
 
 function Profile() {
-	// const { currentUser } = useOutletContext();
-	// const { role } = currentUser;
+	const { currentUser } = useOutletContext();
+	const { role } = currentUser;
 
 	const [isForm, setIsForm] = useState(false);
 
@@ -19,34 +19,34 @@ function Profile() {
 		setIsForm((prev) => !prev);
 	};
 
-	// if (role === 'volunteer') {
-	// if (isForm) {
-	// 	return <ProfileVolunteerEdit handleIsForm={handleIsForm} />;
-	// }
-	// return (
-	// 	<ProfileVolunteer
-	// 		title="Личный кабинет волонтера"
-	// 		handleIsForm={handleIsForm}
-	// 	/>
-	// );
-	// }
-
-	// if (role === 'organizer') {
-	if (isForm) {
+	if (role === 'volunteer') {
+		if (isForm) {
+			return <ProfileVolunteerEdit handleIsForm={handleIsForm} />;
+		}
 		return (
-			<ProfileOrganizationEdit
+			<ProfileVolunteer
+				title="Личный кабинет волонтера"
 				handleIsForm={handleIsForm}
-				organization="ООО «Организация»"
 			/>
 		);
 	}
-	return (
-		<ProfileOrganization
-			title="Личный кабинет организатора"
-			handleIsForm={handleIsForm}
-		/>
-	);
-	// }
+
+	if (role === 'organizer') {
+		if (isForm) {
+			return (
+				<ProfileOrganizationEdit
+					handleIsForm={handleIsForm}
+					organization="ООО «Организация»"
+				/>
+			);
+		}
+		return (
+			<ProfileOrganization
+				title="Личный кабинет организатора"
+				handleIsForm={handleIsForm}
+			/>
+		);
+	}
 }
 
 export default Profile;
