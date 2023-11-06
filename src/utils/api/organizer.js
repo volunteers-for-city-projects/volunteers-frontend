95a33a6fbc3ac7e0a0193233591c8fee7b875b1b
DBKai <dbkai@ya.ru>
2023-11-06 22:48:52 +0500

    feat(organizer): added getProjectById request

--- src/utils/api/organizer.js
+++ src/utils/api/organizer.js
@@ -14,12 +14,29 @@ export async function getProjectCategories() {
 
 export async function createProject(value) {
 	try {
+		const token = localStorage.getItem('token');
 		const response = await fetch(ENDPOINT_ORGANIZER_PROJECTS, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
+				Authorization: `Token ${token}`,
 			},
 			body: JSON.stringify(value),
+		});
+		return response.json();
+	} catch (error) {
+		throw new Error(console.error(error));
+	}
+}
+
+export async function getProjectById(id) {
+	try {
+		const token = localStorage.getItem('token');
+		const response = await fetch(`${ENDPOINT_ORGANIZER_PROJECTS}${id}/`, {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Token ${token}`,
+			},
 		});
 		return response.json();
 	} catch (error) {
