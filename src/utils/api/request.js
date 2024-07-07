f381feb498940ea479b3c87a704ef86374c35a8d
unknown <panterajuliapapina@yandex.ru>
2024-07-07 19:58:03 +0300

    fix(request): change domain name

--- src/utils/api/request.js
+++ src/utils/api/request.js
@@ -1,6 +1,6 @@
 import errorFields from '../errorFields';
 
-export const BASE_URL = 'https://2260993-dk30711.twc1.net/api';
+export const BASE_URL = 'https://better-together.tw1.ru';
 
 const parseErrors = (obj) => {
 	const errorArray = [];
